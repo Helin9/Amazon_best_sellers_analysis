cf5e610c6f0009a169a5a8e8d9339c04136ecf9a
Helin9 <helinmen9@gmail.com>
2026-03-17 15:59:20 +0100

    Change sorting: showing highest ratings first

--- main.py
+++ main.py
@@ -29,9 +29,28 @@ print(author_counts)
 avg_rating_by_genre= df.groupby("Genre")["Rating"].mean()
 print(avg_rating_by_genre)
 
+
+
+# the books with the most reviews categorized by rating (to see the most popular books)
+# 1. handling the year duplicates - keeping the versio of the book with the highest num of reviews ever recorded
+df_unique_books=df.sort_values("Reviews",ascending=False).drop_duplicates(subset=["Title","Author"])
+
+
+# 2. sorting the entire list by rating (ascending) & reviews (descending)- to find the most reviewed book by each rating category
+# and handling duplicate ratings
+df_sorted = df_unique_books.sort_values(by=["Rating","Reviews"], ascending=[False,False])
+df_sorted = df_sorted.drop_duplicates(subset=["Rating"])
+
+# 4. picking the columns we want to see 
+most_popular_books= df_sorted[["Rating","Title","Author","Reviews"]]
+print(most_popular_books)
+
+
 # exporting top selling authors to a csv file
 author_counts.head(10).to_csv("top_authors.csv")
 
 # exporting avg ratings by genre to a csv file
 avg_rating_by_genre.to_csv("avg_ratings_by_genre.csv")
 
+# exporting most_popular_books to a csv file
+most_popular_books.to_csv("most_popular_books.csv",index=False)
